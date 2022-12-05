5914aaaa221b210615d72f850bbf22333da26182
FutileCode <99463618+FutileCode@users.noreply.github.com>
2022-12-05 21:15:39 +0000

    Add files via upload

--- 4/main.go
+++ 4/main.go
@@ -26,11 +26,11 @@ func main() {
 
 	var contains []string
 	re := regexp.MustCompile("[0-9]+")
+
 	for i, v := range lines {
 		r := re.FindAllString(v, -1)
 		p1 := r[:len(r)/2]
 		p2 := r[len(r)/2:]
-		fmt.Println(p1, p2)
 		switch {
 		case p1[0] >= p2[0] && p1[1] <= p2[1]:
 			contains = append(contains, fmt.Sprintf("Pair %v -> %v fully contains %v\n", i, p2, p1))
